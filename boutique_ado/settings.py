9a01e1072e125a36afb9bc64f23146d7a298a792
Satogako <genevaq@ukr.net>
2023-11-26 17:37:52 +0000

    059 - deployment part 6

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,8 +185,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ec-bbd3ab3be3d9.herokuapp.com'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ec-bbd3ab3be3d9'
+    AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
